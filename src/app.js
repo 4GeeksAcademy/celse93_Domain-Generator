ee6e42d325318f31c7c41bdb0ad17877ce17b750
PASM <157536097+celse93@users.noreply.github.com>
2025-06-13 20:58:12 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,12 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ['the', 'our'];
+  let adjs = ['great', 'big'];
+  let nouns = ['jogger', 'racoon'];
+  let extensions = ['.com', '.net', '.us', '.io'];
+
+  let domainGenerator = pronouns.map(pronoun => adjs.map(adj => nouns.map(noun => extensions.map(extension =>
+	  console.log(`${pronoun}${adj}${noun}${extension}`)))));
 };
+
